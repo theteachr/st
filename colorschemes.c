265fb1b34ba47d92b1b6f24ec7d985f639d341f5
theteachr <sop.theteachr@gmail.com>
2022-01-25 00:38:33 +0530

    Collect colorschemes

---
+++ colorschemes.c
@@ -0,0 +1,56 @@
+static const char *catppuccin[] = {
+	/* 8 normal colors */
+	"#6E6C7E",
+	"#F28FAD",
+	"#ABE9B3",
+	"#FAE3B0",
+	"#96CDFB",
+	"#F5C2E7",
+	"#89DCEB",
+	"#C3BAC6",
+
+	/* 8 bright colors */
+	"#988BA2",
+	"#F28FAD",
+	"#ABE9B3",
+	"#FAE380",
+	"#96CDFB",
+	"#F5C2E7",
+	"#89DCEB",
+	"#D9E0EE",
+
+	[255] = 0,
+
+	[256] = "#1E1D2E", // bg
+	[257] = "#D9E0EE", // fg
+	[258] = "#F5E0DC", // cursor
+};
+
+static const char *everforest[] = {
+	/* 8 normal colors */
+	"#323D43",
+	"#E68183",
+	"#A7C080",
+	"#D9BB80",
+	"#83B6AF",
+	"#D39BB6",
+	"#87C095",
+	"#D8CAAC",
+
+	/* 8 bright colors */
+	"#3F4E56",
+	"#EB8C8D",
+	"#B4CF89",
+	"#E2C68C",
+	"#89C6BE",
+	"#DDA3BE",
+	"#8FCE9E",
+	"#E1D2B1",
+
+	[255] = 0,
+
+	/* more colors can be added after 255 to use with DefaultXX */
+	[256] = "#232B2F", // bg
+	[257] = "#ECE4D5", // fg
+	[258] = "#D1495B", // cursor
+};
